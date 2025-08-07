d98c53d9604bfea7c3cb8a574d93535a5d55cbd0
Magnizo10 <thulig.mphungulo@gmail.com>
2025-08-07 22:17:52 +0200

    Finished project

--- TG_Farm.sql
+++ TG_Farm.sql
@@ -1,4 +1,6 @@
-create database TGfarm 
+--create database TGfarm 
+Use TGfarm
+go
 
 create table suppliers(
 supplierID INT primary key identity, 
@@ -37,3 +39,28 @@ foreign key (farmerID) References farmers(FarmerID))
 
 select * from employees
 
+create table equipment(
+equipmentID INT primary key Identity,
+Name varchar(100),
+purchase_date Date,
+status varchar(50))
+
+select * from equipment
+
+create table crop(
+name varchar(100),
+season varchar(50))
+
+select * from crop
+
+
+create table crop_planted(
+crop_planted_id INT Primary Key,
+Crop_id INT,
+plant_date date,
+foreign key(Crop_id) References crop(crop_id))
+
+select * from crop_planted
+
+Alter Table crop
+add crop_id INT primary key identity;
